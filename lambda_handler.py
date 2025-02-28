b5894cf3b606604bf2eecbf12658c5784ff0139c
Cristian Mendivil <cmendivil7@icloud.com>
2025-02-27 21:47:58 -0700

    FLASK

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,8 +1,8 @@
 from mangum import Mangum
-from main import app, asgi_app  # Import your Flask app
+from main import app # Import your Flask app
 
 
 # Create the Magnum handler for Flask
 def lambda_handler(event, context):
-    handler = Mangum(asgi_app)
+    handler = Mangum(app)
     return handler(event, context)
